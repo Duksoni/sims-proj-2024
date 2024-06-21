caa000a7a6f8e8dd89834024edca5bb5c090e13e
Dušan Komadinović <dusan.komadinovic20@gmail.com>
2024-06-21 19:04:38 +0200

    Add classes that represent the users

---
+++ PetNetwork/Domain/Models/Address.cs
@@ -0,0 +1,35 @@
+﻿using PetNetwork.Domain.Interfaces;
+
+namespace PetNetwork.Domain.Models;
+
+public class Address : ISerializable
+{
+
+    public string Id => $"{Street.Replace(' ', '-')}_{StreetNo}";
+    
+    public string Street { get; set; }
+
+    public string StreetNo { get; set; }
+
+    public string Town { get; set; }
+
+    private int _postalCode = 11000;
+    public int PostalCode
+    {
+        get => _postalCode;
+        set
+        {
+            if (value is < 11000 or > 38299)
+                throw new ArgumentException("Postal code must be in range [11000, 38300)");
+            _postalCode = value;
+        }
+    }
+
+    public Address(string street, string streetNo, string town, int postalCode)
+    {
+        Street = street;
+        StreetNo = streetNo;
+        Town = town;
+        PostalCode = postalCode;
+    }
+}
---
+++ PetNetwork/Domain/Models/UserAccount.cs
@@ -0,0 +1,58 @@
+﻿using PetNetwork.Application.Utility;
+using PetNetwork.Domain.Enums;
+using PetNetwork.Domain.Interfaces;
+
+namespace PetNetwork.Domain.Models;
+
+public class UserAccount : ISerializable
+{
+    public string Id { get; }
+
+    private string _password = string.Empty;
+    public string Password
+    {
+        get => _password;
+        set
+        {
+            if (value == string.Empty) throw new ArgumentException("Password can't be empty");
+            _password = PasswordHasher.Hash(value);
+        }
+    }
+
+    private AccountRole _role;
+    public AccountRole Role
+    {
+        get => _role;
+        set
+        {
+            if (value == AccountRole.Admin) throw new ArgumentException("No new admin can be added");
+            _role = value;
+        }
+    }
+
+    public AccountStatus Status { get; private set; }
+
+    public UserAccount(string email, string password)
+    {
+        Id = email;
+        Password = password;
+        Role = AccountRole.RegularUser;
+        Status = AccountStatus.PendingApproval;
+    }
+
+    public UserAccount(string email, string password, AccountRole role)
+    {
+        Id = email;
+        Password = password;
+        Role = role;
+        Status = AccountStatus.PendingApproval;
+    }
+
+    public UserAccount(string id, string password, AccountRole role, AccountStatus status)
+    {
+        Id = id;
+        Password = password;
+        Role = role;
+        Status = status;
+    }
+}
