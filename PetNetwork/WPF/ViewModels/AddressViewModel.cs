ac6b29ad182b09c8355b4841873317477f12e710
Dušan Komadinović <dusan.komadinovic20@gmail.com>
2024-06-21 19:08:07 +0200

    Add ViewModel classes used for user registration

---
+++ PetNetwork/WPF/ViewModels/AddressViewModel.cs
@@ -0,0 +1,59 @@
+﻿using PetNetwork.Domain.Models;
+
+namespace PetNetwork.WPF.ViewModels;
+
+public class AddressViewModel : BaseViewModel
+{
+
+	private string _street = string.Empty;
+    public string Street
+    {
+        get => _street;
+        set
+        {
+            if (_street == value) return;
+            _street = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private string _streetNo = string.Empty;
+    public string StreetNo
+    {
+        get => _streetNo;
+        set
+        {
+            if (_streetNo == value) return;
+            _streetNo = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private string _town = string.Empty;
+    public string Town
+    {
+        get => _town;
+        set
+        {
+            if (_town == value) return;
+            _town = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private int _postalCode = 11000;
+    public int PostalCode
+    {
+        get => _postalCode;
+        set
+        {
+            if (_postalCode == value) return;
+            if (value is < 11000 or > 38299)
+                _postalCode = 11000;
+            _postalCode = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public Address ToAddress() => new(Street, StreetNo, Town, PostalCode);
+}
---
+++ PetNetwork/WPF/ViewModels/PersonViewModel.cs
@@ -0,0 +1,95 @@
+﻿using PetNetwork.Domain.Enums;
+using PetNetwork.Domain.Models;
+using PetNetwork.WPF.ViewModels.Validation;
+
+namespace PetNetwork.WPF.ViewModels;
+
+public class PersonViewModel : BaseViewModel
+{
+    private string _firstName = string.Empty;
+    public string FirstName
+    {
+        get => _firstName;
+        set 
+        {
+            if (_firstName == value) return;
+            _firstName = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private string _lastName = string.Empty;
+    public string LastName
+    {
+        get => _lastName;
+        set 
+        {
+            if (_lastName == value) return;
+            _lastName = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private string _phone = string.Empty;
+    public string Phone
+    {
+        get => _phone;
+        set 
+        {
+            if (_phone == value) return;
+            _phone = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private Gender _gender;
+    public Gender Gender
+    {
+        get => _gender;
+        set 
+        {
+            if (_gender == value) return;
+            _gender = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public AddressViewModel AddressViewModel { get; } = new();
+
+    private string _identityCardNo = string.Empty;
+    public string IdentityCardNo
+    {
+        get => _identityCardNo;
+        set
+        {
+            if (_identityCardNo == value) return;
+            _identityCardNo = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public Person ToPerson(string email) =>
+        new(email, FirstName, LastName, Phone, Gender, AddressViewModel.ToAddress(), IdentityCardNo);
+
+    private readonly PersonInputValidation _validation = new();
+
+    public virtual string this[string columnName]
+    {
+        get
+        {
+            return columnName switch
+            {
+                "FirstName" => _validation.ValidateFirstName(FirstName),
+                "LastName" => _validation.ValidateLastName(LastName),
+                "Phone" => _validation.ValidatePhone(Phone),
+                "Address" => _validation.ValidateAddress(AddressViewModel),
+                "IdentityCardNo" => _validation.ValidateIdentityCardNo(IdentityCardNo),
+                _ => string.Empty
+            };
+        }
+    }
+
+    private readonly string[] _validatedProperties = { "FirstName", "LastName", "Phone", "Address", "IdentityCardNo" };
+
+    public bool IsValid => _validatedProperties.All(property => this[property] == string.Empty);
+}
