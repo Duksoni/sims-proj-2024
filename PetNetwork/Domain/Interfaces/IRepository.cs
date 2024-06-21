7889fc578a921a6bd4fcac9c4185ed6d17e2f84f
Dušan Komadinović <dusan.komadinovic20@gmail.com>
2024-06-21 19:11:01 +0200

    Add IRepository generic interface - Defines all CRUD methods that each repository should have

---
+++ PetNetwork/Domain/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace PetNetwork.Domain.Interfaces;
+
+public interface IRepository<T> where T : ISerializable
+{
+    void Add(T newEntry);
+    void Update(T updatedEntry);
+    void Remove(string id);
+    T? Get(string id);
+    IList<T> GetAll();
+    IList<T> Find(Func<T, bool> predicate);
+}
