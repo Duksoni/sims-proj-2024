8c55ce530e5b36918686ad4bc39b17662e6711d4
Dušan Komadinović <dusan.komadinovic20@gmail.com>
2024-06-25 15:17:10 +0200

    Add UserMessages class - Provides messages when user is logging-in or registering an account

---
+++ PetNetwork/Application/Utility/Constants/UserMessages.cs
@@ -0,0 +1,10 @@
+﻿namespace PetNetwork.Application.Utility.Constants;
+
+public static class UserMessages
+{
+    
+    public const string InvalidLogin = "Invalid email or password. Please try again.";
+    public const string UserNotFound = "User with those credentials doesn't exist. Please try again.";
+    public const string FailedRegistration = "Unable to register. Not all fields are valid.";
+    public const string AccountDisabled = "Your account is disabled";
+}
