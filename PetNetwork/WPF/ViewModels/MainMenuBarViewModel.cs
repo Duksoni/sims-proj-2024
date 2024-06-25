b0db0fca8d1d7bcfcfac13632a03945d759ec665
Dušan Komadinović <dusan.komadinovic20@gmail.com>
2024-06-25 18:40:24 +0200

    Add MainMenuBar View and ViewModel classes - Holds options for login, logout, registration and exit

---
+++ PetNetwork/WPF/ViewModels/MainMenuBarViewModel.cs
@@ -0,0 +1,24 @@
+﻿using PetNetwork.Application.Utility;
+
+namespace PetNetwork.WPF.ViewModels;
+
+public class MainMenuBarViewModel
+{
+    public RelayCommand LoginCommand => new(_ => Login());
+
+    public RelayCommand LogoutCommand => new(_ => Logout());
+
+
+    private static void Login()
+    {
+        var app = System.Windows.Application.Current as App;
+        app?.ResetMainWindow();
+    }
+
+    private static void Logout()
+    {
+        UserSession.Stop();
+        var app = System.Windows.Application.Current as App;
+        app?.ResetMainWindow();
+    }
+}
