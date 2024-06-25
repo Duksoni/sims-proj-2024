b109fe7677b1e67aa97f1a571e14d2afdb5fdc14
Dušan Komadinović <dusan.komadinovic20@gmail.com>
2024-06-25 16:48:01 +0200

    Add RelayCommand class - Can be used in View, by binding Button's command property to command found in ViewModel

---
+++ PetNetwork/WPF/RelayCommand.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Input;
+
+namespace PetNetwork.WPF;
+
+public class RelayCommand : ICommand
+{
+    private readonly Action<object> _execute;
+    private readonly Predicate<object>? _canExecute;
+
+    public event EventHandler? CanExecuteChanged
+    {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
+
+    public RelayCommand(Action<object> execute, Predicate<object>? canExecute = null)
+    {
+        _execute = execute ?? throw new NullReferenceException("Method given to command cannot be null");
+        _canExecute = canExecute;
+    }
+
+    public RelayCommand(Action<object> execute) : this(execute, null) { }
+
+
+    public bool CanExecute(object? parameter = null) => _canExecute == null || _canExecute(parameter);
+
+    public void Execute(object? parameter = null) => _execute.Invoke(parameter);
+}
