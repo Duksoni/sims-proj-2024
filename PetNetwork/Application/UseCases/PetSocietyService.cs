b59c62389df551939b79fd53858cc3fa4a711366
Dušan Komadinović <dusan.komadinovic20@gmail.com>
2024-06-25 17:46:37 +0200

    Add PetSocietyService - Provides only Add, Update and Get methods (should be all that are needed)

---
+++ PetNetwork/Application/UseCases/PetSocietyService.cs
@@ -0,0 +1,28 @@
+﻿using PetNetwork.Domain.Interfaces;
+using PetNetwork.Domain.Models;
+
+namespace PetNetwork.Application.UseCases;
+
+public class PetSocietyService
+{
+    private readonly IRepository<PetSociety> _petSocietyRepository;
+
+    public PetSocietyService(IRepository<PetSociety> petSocietyRepository)
+    {
+        _petSocietyRepository = petSocietyRepository;
+    }
+
+    public void Add(PetSociety society)
+    {
+        if (Get() == null)
+        {
+            _petSocietyRepository.Add(society);
+            return;
+        }
+        Update(society);
+    }
+
+    public void Update(PetSociety updatedSociety) => _petSocietyRepository.Update(updatedSociety);
+
+    public PetSociety? Get() => _petSocietyRepository.GetAll().FirstOrDefault();
+}
