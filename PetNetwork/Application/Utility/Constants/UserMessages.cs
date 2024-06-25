6449a50305c3b823943d6b56b85b079e56250f2f
Dušan Komadinović <dusan.komadinovic20@gmail.com>
2024-06-25 17:45:26 +0200

    Add new messages to UserMessages

--- PetNetwork/Application/Utility/Constants/UserMessages.cs
+++ PetNetwork/Application/Utility/Constants/UserMessages.cs
@@ -7,4 +7,6 @@ public static class UserMessages
     public const string UserNotFound = "User with those credentials doesn't exist. Please try again.";
     public const string FailedRegistration = "Unable to register. Not all fields are valid.";
     public const string AccountDisabled = "Your account is disabled";
+    public const string PendingApproval = "Your account has not yet been approved.";
+    public const string Rejected = "Your application has been rejected.";
 }
