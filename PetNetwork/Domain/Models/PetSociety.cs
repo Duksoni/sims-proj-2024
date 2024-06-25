00a9c5bf9902713c394d60392e1c6ee581658e74
Dušan Komadinović <dusan.komadinovic20@gmail.com>
2024-06-25 15:15:06 +0200

    Add PetSociety model class

---
+++ PetNetwork/Domain/Models/PetSociety.cs
@@ -0,0 +1,15 @@
+﻿namespace PetNetwork.Domain.Models;
+
+public class PetSociety
+{
+    public string Name { get; set; }
+
+    public string BankAccount { get; set; }
+
+    public PetSociety(string name, string bankAccount)
+    {
+        Name = name;
+        BankAccount = bankAccount;
+    }
+
+}
