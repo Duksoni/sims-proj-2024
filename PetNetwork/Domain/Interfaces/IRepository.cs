3c73f1f5019ba51cb36c51615d9841ccce5c4cbd
Dušan Komadinović <dusan.komadinovic20@gmail.com>
2024-06-25 16:44:12 +0200

    Minor change to IRepositry interface

--- PetNetwork/Domain/Interfaces/IRepository.cs
+++ PetNetwork/Domain/Interfaces/IRepository.cs
@@ -6,6 +6,6 @@ public interface IRepository<T> where T : ISerializable
     void Update(T updatedEntry);
     void Remove(string id);
     T? Get(string id);
-    IList<T> GetAll();
+    IList<T> GetAll(bool includeRemoved = false);
     IList<T> Find(Func<T, bool> predicate);
 }
