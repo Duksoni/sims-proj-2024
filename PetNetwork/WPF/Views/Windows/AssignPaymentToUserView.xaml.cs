62c6ab36fd4420d30347fc597d9d798c8f4aa515
Dušan Komadinović <dusan.komadinovic20@gmail.com>
2024-07-01 12:36:03 +0200

    Add AssignPaymentToUser ViewModel and View classes - When importing payments, Payer property can be assigned to a user, or left out as is if Payer is not user of the app

---
+++ PetNetwork/WPF/Views/Windows/AssignPaymentToUserView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using PetNetwork.WPF.ViewModels;
+
+namespace PetNetwork.WPF.Views.Windows;
+
+/// <summary>
+/// Interaction logic for AssignPaymentToUserView.xaml
+/// </summary>
+public partial class AssignPaymentToUserView
+{
+    public AssignPaymentToUserView(PaymentViewModel selectedPayment)
+    {
+        InitializeComponent();
+        var viewModel = new AssignPaymentToUserViewModel(selectedPayment);
+        DataContext = viewModel;
+        viewModel.PropertyChanged += (_, args) => {
+            if (args.PropertyName == nameof(viewModel.Assigned))
+                DialogResult = true;
+        };
+    }
+}
