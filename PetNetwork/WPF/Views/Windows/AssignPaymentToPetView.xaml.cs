3b32808fc9c32f2bdbc9695c8026e1216fe05977
Dušan Komadinović <dusan.komadinovic20@gmail.com>
2024-07-01 14:16:23 +0200

    Add AssignPaymentToPet View and ViewModel - Behaves like assigning payment to user - WIP as Pet class doesn't exist yet

---
+++ PetNetwork/WPF/Views/Windows/AssignPaymentToPetView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using PetNetwork.WPF.ViewModels;
+
+namespace PetNetwork.WPF.Views.Windows;
+
+/// <summary>
+/// Interaction logic for AssignPaymentToPetView.xaml
+/// </summary>
+public partial class AssignPaymentToPetView
+{
+    public AssignPaymentToPetView(PaymentViewModel selectedPayment)
+    {
+        InitializeComponent();
+        var viewModel = new AssignPaymentToPetViewModel(selectedPayment);
+        DataContext = viewModel;
+        viewModel.PropertyChanged += (_, args) => {
+            if (args.PropertyName == nameof(viewModel.Assigned))
+                DialogResult = true;
+        };
+    }
+}
