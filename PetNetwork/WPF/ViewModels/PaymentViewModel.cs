6db6bb8bb0c252833bb82ea677d62568f92054cc
Dušan Komadinović <dusan.komadinovic20@gmail.com>
2024-07-01 15:31:00 +0200

    Update PaymentViewModel - Can now be used for inputting donations

--- PetNetwork/WPF/ViewModels/PaymentViewModel.cs
+++ PetNetwork/WPF/ViewModels/PaymentViewModel.cs
@@ -1,15 +1,16 @@
 ﻿using PetNetwork.Domain.Enums;
 using PetNetwork.Domain.Models;
+using System.ComponentModel;
 
 namespace PetNetwork.WPF.ViewModels;
 
-public class PaymentViewModel : BaseViewModel
+public class PaymentViewModel : BaseViewModel, IDataErrorInfo
 {
     public string Id { get; } = string.Empty;
 
     public string PayerBankAccountNo { get; } = string.Empty;
 
-	private string _payer;
+	private string _payer = string.Empty;
 	public string Payer
 	{
 		get => _payer;
@@ -21,11 +22,43 @@ public class PaymentViewModel : BaseViewModel
 		}
 	}
 
-    public string Purpose { get; }
+    private string _purpose = string.Empty;
+    public string Purpose
+    {
+        get => _purpose;
+        set
+        {
+            if (_purpose == value) return;
+            _purpose = value;
+            OnPropertyChanged();
+        }
+    }
 
-    public decimal Amount { get; }
+    private decimal _amount;
+    public decimal Amount
+    {
+        get => _amount;
+        set
+        {
+            if (_amount == value) return;
+            if (value < 0)
+                value = 0;
+            _amount = value;
+            OnPropertyChanged();
+        }
+    }
 
-	public DateTime PaymentDate { get; }
+    private DateTime _paymentDate = DateTime.Now;
+    public DateTime PaymentDate
+    {
+        get => _paymentDate;
+        set
+        {
+            if (_paymentDate == value) return;
+            _paymentDate = value;
+            OnPropertyChanged();
+        }
+    }
 
     private PaymentType _paymentType;
     public PaymentType PaymentType
@@ -49,15 +82,17 @@ public class PaymentViewModel : BaseViewModel
         }
     }
 
+    public PaymentViewModel() { }
+
     public PaymentViewModel(Payment payment)
     {
         Id = payment.Id;
 		_payer = payment.Payer;
-        Purpose = payment.Purpose;
-        Amount = payment.Amount;
-        PaymentDate = payment.PaymentDate;
-        PaymentType = payment.PaymentType;
-        AssignedPetId = payment.AssignedPetId;
+        _purpose = payment.Purpose;
+        _amount = payment.Amount;
+        _paymentDate = payment.PaymentDate;
+        _paymentType = payment.PaymentType;
+        _assignedPetId = payment.AssignedPetId;
     }
 
     /// <summary> Use when importing from bank statement file </summary>
@@ -70,5 +105,27 @@ public class PaymentViewModel : BaseViewModel
         PaymentDate = payment.VremeIzvrsenja;
     }
 
-	public Payment ToPayment() => new(_payer, Purpose, Amount, PaymentDate, PaymentType, AssignedPetId);
+	public Payment ToPayment() => Id == string.Empty 
+        ? new Payment(_payer, Purpose, Amount, PaymentDate, PaymentType, AssignedPetId)
+        : new Payment(Id, _payer, Purpose, Amount, PaymentDate, PaymentType, AssignedPetId, false);
+    
+    public string Error => string.Empty;
+
+    public string this[string columnName]
+    {
+        get
+        {
+            return columnName switch
+            {
+                "Payer" => Payer == string.Empty ? "You must enter the payer." : string.Empty,
+                "Purpose" => Purpose == string.Empty ? "You must enter the donation purpose." : string.Empty,
+                "Amount" => Amount == 0 ? "You must enter the amount" : string.Empty,
+                _ => string.Empty
+            };
+        }
+    }
+
+    private readonly string[] _validatedProperties = { "Payer", "Purpose", "Amount" };
+
+    public bool IsValid => _validatedProperties.All(property => this[property] == string.Empty);
 }
