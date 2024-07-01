b018ba17c08f8f3fc0e024905cbe36183c506d58
Dušan Komadinović <dusan.komadinovic20@gmail.com>
2024-07-01 12:32:58 +0200

    Add PaymentExtractor and PaymentDTO class - Used to parse the imported json bank stantement file and store it to PaymentDTO class

---
+++ PetNetwork/Domain/Models/PaymentDTO.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+
+namespace PetNetwork.Domain.Models;
+
+/// <summary>
+/// Class modeled after the schema found on <br/>
+/// <see href="https://epp.trezor.gov.rs/docs/user/bank-accounts/sheme/#json"/>
+/// </summary>
+public class PaymentDTO
+{
+    public string SvrhaDoznake { get; } // Payment purpose
+
+    public decimal Iznos { get; } // Amount
+
+    public string NazivZaduzenja { get; } // Payer
+
+    public string RacunZaduzenja { get; } // Payer bank account
+
+    public DateTime VremeIzvrsenja { get; } // Payment approval date
+
+    [JsonConstructor]
+    public PaymentDTO(string svrhaDoznake, decimal iznos, string nazivZaduzenja, string racunZaduzenja, DateTime vremeIzvrsenja)
+    {
+        SvrhaDoznake = svrhaDoznake;
+        Iznos = iznos;
+        NazivZaduzenja = nazivZaduzenja;
+        RacunZaduzenja = racunZaduzenja;
+        VremeIzvrsenja = vremeIzvrsenja;
+    }
+}
