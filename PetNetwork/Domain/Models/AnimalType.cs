87eeda12e62d26e8a82513e31bbf9bf0042a4eb5
SirBoi <stefanlekicsrbo@gmail.com>
2024-07-02 21:28:07 +0200

    Added Pet class and other classes related to it.

---
+++ PetNetwork/Domain/Models/AnimalType.cs
@@ -0,0 +1,17 @@
+﻿namespace PetNetwork.Domain.Models
+{
+    internal class AnimalType
+    {
+        public string Type { get; set; }
+
+        internal AnimalType()
+        {
+            Type = string.Empty;
+        }
+
+        public AnimalType(string type)
+        {
+            Type = type;
+        }
+    }
+}
