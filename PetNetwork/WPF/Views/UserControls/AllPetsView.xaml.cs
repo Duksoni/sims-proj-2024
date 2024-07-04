e87af29a8806352572978bdcee86e746c34246f9
SirBoi <stefanlekicsrbo@gmail.com>
2024-07-04 17:23:14 +0200

    Added AllPetsView and AllPetsViewModel.

---
+++ PetNetwork/WPF/Views/UserControls/AllPetsView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using PetNetwork.WPF.ViewModels;
+using PetNetwork.WPF.Views.Windows;
+
+namespace PetNetwork.WPF.Views.UserControls
+{
+    /// <summary>
+    /// Interaction logic for AllPetsView.xaml
+    /// </summary>
+    public partial class AllPetsView
+    {
+        public AllPetsView()
+        {
+            InitializeComponent();
+            var viewModel = new AllPetsViewModel();
+            DataContext = viewModel;
+        }
+    }
+}
