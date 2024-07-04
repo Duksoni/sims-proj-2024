298f8658ae06d3cfd68e720235cac28f447fc937
SirBoi <stefanlekicsrbo@gmail.com>
2024-07-04 17:24:35 +0200

    Added adding pets functionality.

---
+++ PetNetwork/WPF/Views/Windows/AddPetView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using PetNetwork.WPF.ViewModels;
+using System.Windows;
+
+namespace PetNetwork.WPF.Views.Windows
+{
+    /// <summary>
+    /// Interaction logic for AddPetView.xaml
+    /// </summary>
+    public partial class AddPetView : Window
+    {
+        public AddPetView()
+        {
+            InitializeComponent();
+            var viewModel = new AddPetViewModel();
+            DataContext = viewModel;
+
+            viewModel.PropertyChanged += (_, args) => {
+                if (args.PropertyName == nameof(viewModel.Added))
+                    DialogResult = true;
+            };
+        }
+    }
+}
