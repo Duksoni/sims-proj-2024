9372dd2345914d38e57ed0acd326a6fbf80c753f
SirBoi <stefanlekicsrbo@gmail.com>
2024-07-04 17:25:48 +0200

    Added AnimalTypeService.

---
+++ PetNetwork/Application/UseCases/AnimalTypeService.cs
@@ -0,0 +1,27 @@
+﻿using PetNetwork.Domain.Interfaces;
+using PetNetwork.Domain.Models;
+
+namespace PetNetwork.Application.UseCases
+{
+    class AnimalTypeService
+    {
+        private readonly IRepository<AnimalType> _animalTypeRepository;
+
+
+        public AnimalTypeService(IRepository<AnimalType> animalTypeRepository)
+        {
+            _animalTypeRepository = animalTypeRepository;
+        }
+
+
+        public void Add(AnimalType type) => _animalTypeRepository.Add(type);
+
+        public void Remove(string id) => _animalTypeRepository.Remove(id);
+
+        public void Update(AnimalType type) => _animalTypeRepository.Update(type);
+
+        public AnimalType? Get(string id) => _animalTypeRepository.Get(id);
+
+        public IList<AnimalType> GetAll(bool includeRemoved = false) => _animalTypeRepository.GetAll(includeRemoved);
+    }
+}
